c0c9aec0f42040e5c71cd8aa193bd70429b68294
Dilshara Sandeepani <99608488+it21164576@users.noreply.github.com>
2022-02-15 08:36:24 +0000

    c ++ t2

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,18 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no){
+  long fac=1;
+  int i;
+  for(i=2;i<=no;i++)
+    {
+    fac=fac*i;
+    }
+  return fac;
+}
+
+long nCr(int n, int r){
+  int nCr;
+  nCr=Factorial(n)/(Factorial(r)*Factorial(n-r));
+  return nCr;
+}
